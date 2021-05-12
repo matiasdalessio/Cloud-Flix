55bd1d24858584c9c358860acd4fb31e3b240609
EmaCuvillier <cuvillieremanuel7@gmail.com>
2021-05-12 20:18:55 -0300

    carousel header, footer acomodado, fetcheo serie

---
+++ frontend/src/components/Carrousel.js
@@ -0,0 +1,176 @@
+import Slider from 'infinite-react-carousel';
+
+const Carrousel = ()=>{
+    const settings =  {
+        pauseOnHover: false,
+        autoplay: true,
+        autoplaySpeed: 3000
+    };
+    return (
+        <div className='contenedorCarrousel'>
+            <Slider >
+                <div >
+                    <div className="section">
+                        <div className="hero-slide-item" style={{backgroundImage:'url("../images/transformer-banner.jpg")'}}>
+                            
+                            <div className="overlay"></div>
+                            <div className="hero-slide-item-content">
+                                <div className="item-content-wraper">
+                                    <div className="item-content-title">
+                                        Transformer
+                                    </div>
+                                    <div className="movie-infos">
+                                        <div className="movie-info">
+                                            <i className="bx bxs-star"></i>
+                                            <span>9.5</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <i className="bx bxs-time"></i>
+                                            <span>120 mins</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>HD</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>16+</span>
+                                        </div>
+                                    </div>
+                                    <div className="item-content-description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, possimus eius. Deserunt non odit, cum vero reprehenderit laudantium odio vitae autem quam, incidunt molestias ratione mollitia accusantium, facere ab suscipit.
+                                    </div>
+                                    <div className="item-action">
+                                        <p className="btn btn-hover">
+                                            <i className="bx bxs-right-arrow"></i>
+                                            <span>Watch now</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div >
+                    <div className="section">
+                        <div className="hero-slide-item" style={{backgroundImage:'url("../images/wanda-banner.jpg")'}}>
+                            
+                            <div className="overlay"></div>
+                            <div className="hero-slide-item-content">
+                                <div className="item-content-wraper">
+                                    <div className="item-content-title">
+                                        Wandavision
+                                    </div>
+                                    <div className="movie-infos">
+                                        <div className="movie-info">
+                                            <i className="bx bxs-star"></i>
+                                            <span>8.5</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <i className="bx bxs-time"></i>
+                                            <span>110 mins</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>HD</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>13+</span>
+                                        </div>
+                                    </div>
+                                    <div className="item-content-description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, possimus eius. Deserunt non odit, cum vero reprehenderit laudantium odio vitae autem quam, incidunt molestias ratione mollitia accusantium, facere ab suscipit.
+                                    </div>
+                                    <div className="item-action">
+                                        <p className="btn btn-hover">
+                                            <i className="bx bxs-right-arrow"></i>
+                                            <span>Watch now</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div >
+                <div >
+                    <div className="section">
+                        <div className="hero-slide-item" style={{backgroundImage:'url("../images/supergirl-banner.jpg")'}}>
+                            
+                            <div className="overlay"></div>
+                            <div className="hero-slide-item-content">
+                                <div className="item-content-wraper">
+                                    <div className="item-content-title">
+                                        Supergirl
+                                    </div>
+                                    <div className="movie-infos">
+                                        <div className="movie-info">
+                                            <i className="bx bxs-star"></i>
+                                            <span>9.0</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <i className="bx bxs-time"></i>
+                                            <span>120 mins</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>HD</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>16+</span>
+                                        </div>
+                                    </div>
+                                    <div className="item-content-description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, possimus eius. Deserunt non odit, cum vero reprehenderit laudantium odio vitae autem quam, incidunt molestias ratione mollitia accusantium, facere ab suscipit.
+                                    </div>
+                                    <div className="item-action">
+                                        <p className="btn btn-hover">
+                                            <i className="bx bxs-right-arrow"></i>
+                                            <span>Watch now</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div  >
+                    <div className="section">
+                        <div className="hero-slide-item" style={{backgroundImage:'url("../images/black-banner.png")'}}>
+                            
+                            <div className="overlay"></div>
+                            <div className="hero-slide-item-content">
+                                <div className="item-content-wraper">
+                                    <div className="item-content-title">
+                                        Black Panther
+                                    </div>
+                                    <div className="movie-infos">
+                                        <div className="movie-info">
+                                            <i className="bx bxs-star"></i>
+                                            <span>9.5</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <i className="bx bxs-time"></i>
+                                            <span>120 mins</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>HD</span>
+                                        </div>
+                                        <div className="movie-info">
+                                            <span>16+</span>
+                                        </div>
+                                    </div>
+                                    <div className="item-content-description">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, possimus eius. Deserunt non odit, cum vero reprehenderit laudantium odio vitae autem quam, incidunt molestias ratione mollitia accusantium, facere ab suscipit.
+                                    </div>
+                                    <div className="item-action">
+                                        <p className="btn btn-hover">
+                                            <i className="bx bxs-right-arrow"></i>
+                                            <span>Watch now</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Slider>
+        </div>
+    )
+}
+export default Carrousel
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,31 @@
 import React from "react"
+import Carrousel from "../components/Carrousel"
 import Footer from "../components/Footer"
-import Hero from "../components/Hero"
 import Lastest from "../components/Lastest"
+import seriesAction from "../redux/actions/seriesAction"
+import { connect } from "react-redux"
 
 
 class Home extends React.Component{
-
     toTop= () => {window.scroll({
         top:0,
         left:0,
         behavior:"smooth"
     })}
+    state={
+        series:[]
+    }
+    componentDidMount(){
+        this.props.fetchSeries()
+        .then( data =>{
+            this.setState({ ...this.state, series: data })
+        })
+    }
 
-    
     render() {
-         
         return(
             <div>
-                <Hero />
+                <Carrousel />
                 <Lastest Lastest={'Movies'}/>
                 <Lastest Lastest={'Series'}/>
                 <Footer/>
@@ -25,5 +33,8 @@ class Home extends React.Component{
         )
     }
 }
+const mapDispatchToProps ={
+    fetchSeries: seriesAction.fetchSeries,
+}
 
-export default Home
+export default connect(null, mapDispatchToProps) (Home)
